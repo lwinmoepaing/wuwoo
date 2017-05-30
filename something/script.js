4cd1af0b5471b4aa56e658f66704ef60e67ee8f4
wuwoo <lwinmoepaing007@gmail.com>
2017-05-31 02:59:36 +0630

    Static small Web

--- something/script.js
+++ something/script.js
@@ -3,16 +3,14 @@ $(document).ready(function(){
 	$("#nav-2 ul li").hover(function(){
 		$(this).css({
 			"transition":"all 0.85s linear",
-			"background":"black",
+			"background":"white",
 			"border":"1px black solid",
-			"color":"white"
 		});
 	},function(){
 		$(this).css({
 			"transition":"all 0.85s linear",
-			"background":"black",
+			"background":"#ddd",
 			"border":"1px gold solid",
-			"color":"gold"
 		});
 	});
 
